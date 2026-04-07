8dae53a0f5c9ce606fd91b09c56fe2c0bcbab60d
chaitanya jadhab <chaitanya0jadhav1@gmail.com>
2026-04-08 02:16:25 +0530

    token blacklisting and logout user , get-me api call to fetch user details

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,8 @@
 //2nd approach
 const {Router}=require("express")
 const authController=require("../controllers/auth.controller")
+const authMiddleware=require("../middlewares/auth.middleware")
+
 const  authRouter=Router()
 
 /**
@@ -22,5 +24,23 @@ authRouter.post("/register",authController.registerUserController)
  */
 
 authRouter.post("/login",authController.loginUserController)
+
+
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add then token in blacklist 
+ * @access public 
+ */
+
+authRouter.get("/logout",authController.logoutUserController)
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private 
+ */
+
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
+
 
 module.exports=authRouter
